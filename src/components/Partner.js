61c2ba1bd989133c5ceda54aa5f8da278ce92060
Annika Hallerberg <annika.hallerberg@gmx.de>
2022-03-23 13:30:07 +0100

    fix: exchanged fonts, updated partner section

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -9,7 +9,8 @@ const Partner = () => {
           Ein Netzwerk voller <span>Support</span>.
         </Title>
         <div className="contentWer">
-              <StaticImage src='../assets/images/ubg.jpeg' style={{width: '50%'}}></StaticImage>
+              <StaticImage alt="ubg-logo" src='../assets/images/ubg.jpeg' style={{width: '50%'}}></StaticImage>
+              <StaticImage alt="anstiftung logo" src='../assets/images/anstiftung.jpg' style={{width: '50%'}}></StaticImage>
         </div>
       </section>
     );
