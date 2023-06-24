86e6bf23426f844bbe142f7f6548e0424729345b
Annika Hallerberg <annikah@spotify.com>
2023-06-24 21:27:19 +0200

    added the missing freiluftkino part, and disabled status

--- src/components/Kalender.js
+++ src/components/Kalender.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Title } from "./common/Title";
 import Arrow from "../assets/svgs/spritesheet";
 
@@ -26,15 +26,15 @@ const Kalender = ({ data }) => {
   const daysToAddOnFront = Array(firstDayOfTheMonth).fill("");
   daysInAMonthArray.unshift(daysToAddOnFront);
 
-  const filterDataForEvent = (keyword) =>
+  const filterDataByEventType = (keyword) =>
     data
       .filter((date) => months[new Date(date.start).getMonth()] === month)
       .filter((date) => date[keyword])
       .map((date) => new Date(date.start).getDate());
 
-  const gardendays = filterDataForEvent("gartentag");
-  const cinemadays = filterDataForEvent("cinema");
-  const cafedays = filterDataForEvent("cafe");
+  const gardendays = filterDataByEventType("gartentag");
+  const cinemadays = filterDataByEventType("cinema");
+  const cafedays = filterDataByEventType("cafe");
 
   const addCalendarClasses = (chunkBit) => {
     if (gardendays.includes(chunkBit)) {
@@ -47,7 +47,7 @@ const Kalender = ({ data }) => {
     return "kalender__tag";
   };
 
-  // Nodes
+  // nodes
   const weekdaysNodes = weekdays.map((weekday, _) => (
     <td key={_} className="kalender__weekday">
       {weekday}
@@ -70,18 +70,36 @@ const Kalender = ({ data }) => {
     );
   }
 
+  const [leftButtonStyle, setleftButtonStyle] = useState([
+    "kalender__leftArrow",
+  ]);
+  const [rightButtonStyle, setrightButtonStyle] = useState(
+    "kalender__rightArrow"
+  );
   const handleOnLeftClick = (e) => {
     if (months.indexOf(month) === 0) return;
+    if (months.indexOf(month) === 1) {
+      setleftButtonStyle(["kalender__leftArrow disabled"]);
+    } else {
+      setleftButtonStyle(["kalender__leftArrow"]);
+      setrightButtonStyle(["kalender__rightArrow"]);
+    }
     setMonth(months[months.indexOf(month) - 1]);
   };
   const handleOnRightClick = (e) => {
     if (months.indexOf(month) === 11) return;
+    if (months.indexOf(month) === 10) {
+      setrightButtonStyle(["kalender__rightArrow disabled"]);
+    } else {
+      setrightButtonStyle(["kalender__rightArrow"]);
+      setleftButtonStyle(["kalender__leftArrow"]);
+    }
     setMonth(months[months.indexOf(month) + 1]);
   };
 
   return (
     <section className="wrapper utopie">
-      <Title className="blogPageTitle">Veranstaltungen</Title>
+      <Title title="Kalender" className="blogPageTitle">Events</Title>
 
       <table className="kalender">
         <caption className="kalender__caption">{month}</caption>
@@ -96,10 +114,10 @@ const Kalender = ({ data }) => {
         <p className="kalender__legendItem">Café</p>
       </div>
       <div className="kalender__controls">
-        <button className="kalender__leftArrow" onClick={handleOnLeftClick}>
+        <button className={leftButtonStyle} onClick={handleOnLeftClick}>
           <Arrow />
         </button>
-        <button className="kalender_rightArrow" onClick={handleOnRightClick}>
+        <button className={rightButtonStyle} onClick={handleOnRightClick}>
           <Arrow />
         </button>
       </div>
